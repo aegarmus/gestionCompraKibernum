c9711d8a3ba3f462ddaba9c3d5db8b5a060aafa3
Alan Garcia <aegarmus@gmail.com>
2026-02-20 20:38:47 -0300

    feat: hero section created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 console.log('Estamos conectados')
+
+/* 
+
+Crear un sistema que simule la compra venta, creación, edición y eliminación de productos,
+tanto en la gestión de administración como simulación de cliente empleando un falso CRUD
+
+C -> Create
+R -> Read
+U -> Update
+D -> Delete
+
+
+
+*/
